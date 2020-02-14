4c1855b471d530565a6f28dea4a27c12c6c00fda
vrramosm <vf.ramos7@gmail.com>
2020-02-13 21:09:31 -0300

    Updates

---
+++ precache-manifest.68d9951510e1828faa42843ec1b8b07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eade75c71df9eadb8fe27a7d24df1a1",
+    "url": "/localiza-project/index.html"
+  },
+  {
+    "revision": "9c2ad69c106ed70700ab",
+    "url": "/localiza-project/static/css/main.6c684a23.chunk.css"
+  },
+  {
+    "revision": "970ed5776ec62763eaa3",
+    "url": "/localiza-project/static/js/2.9c15775c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/localiza-project/static/js/2.9c15775c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2ad69c106ed70700ab",
+    "url": "/localiza-project/static/js/main.ffd8d738.chunk.js"
+  },
+  {
+    "revision": "5b8faa120bd5e2ce6c10",
+    "url": "/localiza-project/static/js/runtime-main.1a2fedad.js"
+  }
+]);
